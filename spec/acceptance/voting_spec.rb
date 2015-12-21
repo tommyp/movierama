0f4aa4258c584d7402c2449f85201cde7d67bb1e
Tommy Palmer <hi@tommyp.org>
2015-12-21 16:18:48 +0000

    Add failing assertions for voting mailers being sent
    The code to make this pass will come later.

--- spec/acceptance/voting_spec.rb
+++ spec/acceptance/voting_spec.rb
@@ -20,6 +20,8 @@ RSpec.describe 'vote on movies', type: :feature do
       date:         '1980-05-21',
       user:         author
     )
+
+    ActionMailer::Base.deliveries = []
   end
 
   context 'when logged out' do
@@ -39,11 +41,17 @@ RSpec.describe 'vote on movies', type: :feature do
     it 'can like' do
       page.like('Empire strikes back')
       expect(page).to have_vote_message
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      expect(ActionMailer::Base.deliveries.first.subject).to eq('John McFoo liked Empire strikes back')
+      expect(ActionMailer::Base.deliveries.first.to).to include('bob@movierama.dev')
     end
 
     it 'can hate' do
       page.hate('Empire strikes back')
       expect(page).to have_vote_message
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      expect(ActionMailer::Base.deliveries.first.subject).to eq('John McFoo hated Empire strikes back')
+      expect(ActionMailer::Base.deliveries.first.to).to include('bob@movierama.dev')
     end
 
     it 'can unlike' do
@@ -77,6 +85,3 @@ RSpec.describe 'vote on movies', type: :feature do
   end
 
 end
-
-
-
